9ab9fa4e245979ffbe4deaa9979295f6886c543a
anshul <anshulgite2000@gmail.com>
2026-04-29 17:35:32 +0530

    fix: Correct import statement for logger module in exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import logging
-from src.logger import LOG_FILE_PATH
+from  logger import LOG_FILE_PATH
 import sys
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()

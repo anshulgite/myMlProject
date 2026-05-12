82c608ea9d24117636cda6aa0f97429fe215c594
anshul gite <anshul.wiselap@gmail.com>
2026-05-12 12:33:27 +0530

    Add model trainer and evaluation functionality

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,7 +11,7 @@ from src.logger import logging
 from sklearn.model_selection import train_test_split
 
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.components.modle_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -47,7 +47,13 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj=DataIngestion()
-    train_data, test_data, _ = obj.initiate_data_ingestion()
+    train_data_path, test_data_path, _ = obj.initiate_data_ingestion()
+
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data_path, test_data_path
+    )
     logging.info("Data ingestion and data transformation completed")
+
+    model_trainer=ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
